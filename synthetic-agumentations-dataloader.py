537163dab7639c8490c6e4ae3432b488a298eefb
Lígia Melo <61137168+ligiamelo@users.noreply.github.com>
2022-07-26 16:50:36 -0300

    update synthetic augmentations dataset

--- synthetic-agumentations-dataloader.py
+++ synthetic-agumentations-dataloader.py
@@ -18,13 +18,13 @@ from os2d.structures.transforms import TransformList, crop
 
 
 class SyntheticAugmentationsDataset(Dataset):
-    def __init__(self, reference_path, logo_path, box_coder):
+    def __init__(self, reference_path, class_path, box_coder):
         self.reference_path = reference_path
-        self.logo_path = logo_path
+        self.class_path = class_path
         self.reference_images = os.listdir(reference_path)
         self.reference_images.sort()
-        self.logo_images = os.listdir(logo_path)
-        self.logo_images.sort()
+        self.class_images = os.listdir(class_path)
+        self.class_images.sort()
         self.box_coder = box_coder
         self.data_augmentation = DataAugmentation(random_flip_batches=False,
                                       random_crop_size=FeatureMapSize(w=600, h=600),
@@ -33,46 +33,110 @@ class SyntheticAugmentationsDataset(Dataset):
                                       scale_jitter=0.7,
                                       random_color_distortion=True,
                                       random_crop_label_images=False,
-                                      min_box_coverage=0.7)    
+                                      min_box_coverage=0.7) 
+        self.class_idx = 0
         
     
     def __len__(self):
-        return len(self.reference_images) * len(self.logo_images)
-
-    def __getitem__(self, idx):
-        reference_image = Image.open(os.path.join(self.reference_path, self.reference_images[idx // len(self.logo_images)])).convert("RGB")
-        logo = Image.open(os.path.join(self.logo_path, self.logo_images[idx % len(self.logo_images)])).convert("RGBA")
+        return len(self.reference_images) #* len(self.class_images)
+    
+    def _get_image(self, image_idx, image_type):
+        assert image_type.lower() in ["reference", "class"], "Invalid image type."
+        if image_type == "reference":
+            return Image.open(os.path.join(self.reference_path, self.reference_images[image_idx])).convert("RGBA")
+        elif image_type == "class":
+            return Image.open(os.path.join(self.class_path, self.class_images[image_idx])).convert("RGBA")
         
-        ratio = random.uniform(0.075, 0.5)
-        
-        x_ref, y_ref = reference_image.size
-        x_logo, y_logo = logo.size
-        
-        x_new = int(ratio * x_ref)
-        y_new = int(ratio * y_logo * x_ref / x_logo)
-        
-        logo = logo.resize((int(ratio * x_ref), int(ratio * y_logo * x_ref / x_logo)))
-        
-        x_corner = int(random.uniform(-x_new/2, x_ref - x_new/2))
-        y_corner = int(random.uniform(-y_new/2, y_ref - y_new/2))
-        
-        angle = random.uniform(-90, 90)
-        
+    def _add_colored_background(self, img, bg_color=None, mode="RGBA"):
+        assert mode in ["RGB", "RGBA"], "Invalid mode."
+        if bg_color is not None:
+            assert len(bg_color) == len(mode)
+        else:
+            bg_color = tuple(np.random.choice(range(256), size=3))
+            if mode == "RGBA":
+                bg_color = tuple(list(bg_color) + [255])
+        colored_bg = Image.new(mode, img.size, bg_color)
+        colored_bg.paste(img, mask=img.split()[3])
+        return colored_bg
+    
+    def _sample_object_placement(self, img_size, object_size, max_occlusion=0.25):
+        x = int(random.uniform(-object_size[0] * max_occlusion, img_size[0] - (1 - max_occlusion) * object_size[1]))
+        y = int(random.uniform(-object_size[1] * max_occlusion, img_size[1] - (1 - max_occlusion) * object_size[1]))
+        return x, y
+    
+    def _sample_angle(self, min_angle=-45, max_angle=45, p=0.5):
         if random.uniform(0, 1) > 0.5:
-            logo = logo.rotate(angle, expand=1)
+            angle = random.uniform(-45, 45)
         else:
             angle = 0.0
-            
-        reference_image.paste(logo, (x_corner, y_corner), logo)
+        return angle
+    
+    def _random_distort(self, img):
+        if random.uniform(0, 1) > 0.75:
+            img = img.filter(ImageFilter.GaussianBlur(radius=1.5))
+        if random.uniform(0, 1) > 0.5:
+            factor = random.uniform(0.75, 1.25)
+            enhancer = ImageEnhance.Brightness(img)
+            img = enhancer.enhance(factor)
+        if random.uniform(0, 1) > 0.5:
+            factor = random.uniform(0.0, 2.0)
+            enhancer = ImageEnhance.Color(img)
+            img = enhancer.enhance(factor)
+        return img
+
+    def __getitem__(self, idx):
+        # class image modifications to add:
+        #   - background colors for logo
+        #   - different aspect ratios (not high priority)
         
-        white_bg = Image.new("RGB", logo.size, (255,255,255))
-        white_bg.paste(logo)
-        logo = white_bg
+        # input image:
+        #   - background colors for logo
+        #     - sample RGBA with opacity
+        #     - get average color, get complementary background and sample something around it
+        #     - sample light/dark backgrounds
+        #   - rotate logo with 0.5 prob (-45,45) angles
+        #   - resizing the logo 
+        #   - pasting the logo
+        #   - crop logo
+        #   - gaussian noise, saturation, brightness, etc
+        self.class_idx = (self.class_idx + 1) % len(self.class_images)
+        class_id = idx % len(self.class_images)
+        image_id = idx 
         
-        class_id = idx % len(self.logo_images)
-        class_ids = [idx % len(self.logo_images)]
-        image_id = idx // len(self.logo_images)
-        image_ids = [idx // len(self.logo_images)]
+        reference_image = self._get_image(image_id, "reference")
+        class_img = self._get_image(class_id, "class")
+        class_img_to_paste = copy.deepcopy(class_img)
+        
+        class_img_to_paste = self._add_colored_background(class_img_to_paste)
+        class_img = self._add_colored_background(class_img)
+  
+
+        w_ref, h_ref = reference_image.size
+        w_class, h_class = class_img_to_paste.size
+        ratio = random.uniform(0.075, 0.55)
+        
+        h_new, w_new = get_image_size_after_resize_preserving_aspect_ratio(h=h_class, w=w_class, target_size=int(ratio * np.sqrt(w_ref * h_ref)))
+        class_img_to_paste = class_img_to_paste.resize((w_new, h_new))
+
+        angle = self._sample_angle()
+        class_img_to_paste = class_img_to_paste.rotate(angle, expand=1)
+        
+        x_corner, y_corner = self._sample_object_placement((w_ref, h_ref), (w_new, h_new))
+        reference_image.paste(class_img_to_paste, (x_corner, y_corner), class_img_to_paste)
+        
+        reference_image = self._random_distort(reference_image)
+        
+        class_img = class_img.convert("RGB")
+        reference_image = reference_image.convert("RGB")
+        
+        rad = np.radians(angle)
+        x_left = max(0, x_corner)
+        x_right = min(x_corner + np.abs(w_new * np.cos(rad)) + np.abs(h_new * np.sin(rad)), w_ref)
+        y_top = max(0, y_corner)
+        y_bottom = min(y_corner + np.abs(h_new * np.cos(rad)) + np.abs(w_new * np.sin(rad)), h_ref)
+        bbox = torch.tensor([[x_left, y_top, x_right, y_bottom]])
+        
+        
         
         # decide on image level data augmentation
         vflip = random.random() < 0.5 if self.data_augmentation.batch_random_vflip else False
@@ -80,44 +144,31 @@ class SyntheticAugmentationsDataset(Dataset):
         
         # prepare class images
         num_classes = 1
-        logo_th = self._transform_image_gt(logo, hflip=hflip, vflip=vflip)
+        logo_th = self._transform_image_gt(class_img, hflip=hflip, vflip=vflip)
+
         # get the image sizes after resize in self._transform_image_gt, format - width, height
-        logo_size = FeatureMapSize(img=logo)
-        
-        rad = np.radians(angle)
-        x_left = max(0, x_corner)
-        x_right = min(x_corner + np.abs(x_new * np.cos(rad)) + np.abs(y_new * np.sin(rad)), x_ref)
-        y_top = max(0, y_corner)
-        y_bottom = min(y_corner + np.abs(y_new * np.cos(rad)) + np.abs(x_new * np.sin(rad)), y_ref)
-        
-        bbox = torch.tensor([[x_left, y_top, x_right, y_bottom]])
-         
-         # prepare images and boxes
-        batch_box_inverse_transform = []
-        batch_boxes = []
-        batch_img_size = []
-        
+        logo_size = FeatureMapSize(img=class_img)
+       
         # get annotation
         fm_size = FeatureMapSize(img=reference_image)
         boxes = BoxList(bbox, image_size=fm_size, mode="xyxy")
-        boxes.add_field("labels", torch.tensor([idx % len(self.logo_images)]))
-        boxes.add_field("labels_original", torch.tensor([idx % len(self.logo_images)]))
+        boxes.add_field("labels", torch.tensor([class_id]))
+        boxes.add_field("labels_original", torch.tensor([class_id]))
 
         # convert global indices to local
-        self.update_box_labels_to_local(boxes, class_ids)
+        self.update_box_labels_to_local(boxes, [class_id])
 
         # prepare image and boxes: convert image to tensor, data augmentation: some boxes might be cut off the image
         image_mined_data = None 
+
         reference_image_th, boxes, mask_cutoff_boxes, mask_difficult_boxes, box_inverse_transform = \
                  self._transform_image(copy.deepcopy(reference_image), boxes, hflip=hflip, vflip=vflip, mined_data=image_mined_data)
-
+      
         img_size = FeatureMapSize(img=reference_image_th)
         
         boxes.get_field("labels")[mask_cutoff_boxes] = -2
         loc_targets, class_targets = box_coder.encode(boxes, img_size, num_classes)
         
-        #print(type(reference_image_th), type(logo_th), type(loc_targets), type(class_targets))
-        #print(type(class_id), type([logo_size]), type([box_inverse_transform]), type(boxes), type([img_size]))
         return reference_image_th, logo_th, loc_targets, class_targets, class_id, logo_size, \
                box_inverse_transform, boxes, img_size 
     
@@ -144,6 +195,7 @@ class SyntheticAugmentationsDataset(Dataset):
         transforms_th = transforms.Compose([transforms.ToTensor(),
                                             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
         img = transforms_th(img)
+
         return img
 
     def convert_label_ids_global_to_local(self, label_ids_global, class_ids):
@@ -174,7 +226,6 @@ class SyntheticAugmentationsDataset(Dataset):
                                           pyramid_scales=(1,),
                                           mined_data=None ):
         img_size = FeatureMapSize(img=img)
-
         num_pyramid_levels = len(pyramid_scales)
 
         if boxes is None:
@@ -190,6 +241,7 @@ class SyntheticAugmentationsDataset(Dataset):
 
         if do_augmentation:        
             if self.data_augmentation.do_random_crop:
+
                 img, boxes, mask_cutoff_boxes, mask_difficult_boxes = \
                             self.data_augmentation.random_crop(img,
                                                                boxes=boxes,
@@ -198,8 +250,8 @@ class SyntheticAugmentationsDataset(Dataset):
                 img, boxes = transforms_boxes.resize(img, target_size=self.data_augmentation.random_crop_size,
                                                      random_interpolation=self.data_augmentation.random_interpolation,
                                                      boxes=boxes,
-                                                     transform_list=box_inverse_transform)
-
+                                                   transform_list=box_inverse_transform)
+            
             # color distortion
             img = self.data_augmentation.random_distort(img)
 
@@ -218,14 +270,13 @@ class SyntheticAugmentationsDataset(Dataset):
             pyramid_box_inverse_transform.append(box_inverse_transform_this_scale)
             img_pyramid.append( p_img )
             boxes_pyramid.append( p_boxes )
-
+            
         transforms_th = [transforms.ToTensor()]
         if img_normalization is not None:
             transforms_th += [transforms.Normalize(img_normalization["mean"], img_normalization["std"])]
 
         for i_p in range(num_pyramid_levels):
             img_pyramid[i_p] = transforms.Compose(transforms_th)( img_pyramid[i_p] )
-
         return img_pyramid, boxes_pyramid, mask_cutoff_boxes, mask_difficult_boxes, pyramid_box_inverse_transform
       
  def collate_fn(data):
